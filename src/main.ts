f19263b39386402d627983e18021db73aff06aa0
SmartBest_Yang <276996733@qq.com>
2024-05-31 11:41:59 +0800

    Initial commit.

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Vxe Table
+import VxeUITable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+// i118n
+import { createI18n } from 'vue-i18n';
+import messages from '@/language/index'
+const i18n = createI18n({
+  legacy: false,  // 设置为 false，启用 composition API 模式
+  messages,
+  locale: 'en'  // 设置默认语言
+})
+
+// Ant Design Vue
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
+createApp(App)
+.use(store)
+.use(router)
+.use(i18n)
+.use(VxeUITable)
+.use(Antd)
+.mount('#app')
